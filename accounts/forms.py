ea9af8f29d6dda201a12f16deb0ca40c0e21e256
Cova <acovamartinez@gmail.com>
2023-05-14 11:55:23 -0600

    cambios
    eliminación de login

--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Account
-
-class AccountCreate(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = Account
-        fields = '__all__'
